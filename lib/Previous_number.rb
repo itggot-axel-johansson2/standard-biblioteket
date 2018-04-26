48655877e1621f63d5cb75872831539f5c5bb9e2
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-04-26 14:12:07 +0200

    tomdoc

--- lib/Previous_number.rb
+++ lib/Previous_number.rb
@@ -1,3 +1,13 @@
+# Public: shows the previous number
+#
+# number: the first number
+#
+# Exaples
+#
+# next_number(8)
+#  #=> 7
+#
+# returns the previous number
 def previous_number(number)
     output = number - 1
 end
