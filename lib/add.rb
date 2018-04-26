90fc12595fff5de3f2970601921e7da659bde067
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-04-26 14:06:12 +0200

    tomdoc

--- lib/add.rb
+++ lib/add.rb
@@ -1,3 +1,15 @@
-def add(term1, term 2)
+# Public: add two numbers
+#
+# term1: the first integer
+#
+# term2: the second integer
+#
+# Exaples
+#
+# add(5, 3)
+#  #=> 8
+#
+# returns add two numbers
+def add(term1, term2)
     return term1 + term2
 end 
