b671b815ebffc4cbfcb24470ab8f177ef1ea734b
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-04-26 14:01:27 +0200

    tom doc

--- lib/is_even.rb
+++ lib/is_even.rb
@@ -1,3 +1,13 @@
+# Public: Check if the number is even
+#
+# number: the integer that will be checked
+#
+# Examples
+#
+# is_even(20)
+#  #=> true
+#
+# returns true if the number is  even
 def is_even(number)
     output = false
     if number % 2 ==0
