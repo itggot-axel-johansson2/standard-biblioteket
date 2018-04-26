b46ec94dc1a8081abf42ff202c735d59073032c7
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-04-26 14:07:45 +0200

    tom doc

--- lib/cube.rb
+++ lib/cube.rb
@@ -1,3 +1,13 @@
+# Public: make a number cube
+#
+# number: the integer that will be cubed
+#
+# Exaples
+#
+# cube(5)
+#  #=> 125
+#
+# returns the number in cube
 def cube(number)
     output = number * number * number 
 end
