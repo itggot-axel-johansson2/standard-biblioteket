4947e4f207050b409c92fea69b33e14e7c10d204
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-04-26 14:02:53 +0200

    tom doc

--- lib/Absolute.rb
+++ lib/Absolute.rb
@@ -1,3 +1,13 @@
+# Public: Check if the number is absolute
+#
+# number: the integer that will be checked
+#
+# Exaples
+#
+# is_absolute(-5)
+#  #=> 5
+#
+# returns the absolute number
 def is_absolute(number)
     output= false
     if number > 0
