707b9e0d69a0c069127f4a87e06220ed1b001207
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-05-03 14:53:55 +0200

    last

---
+++ dev/last_of.rb
@@ -0,0 +1,14 @@
+# Public: shows the the last number in the array
+#
+# array: input
+#
+# Examples
+#
+# first_of([5,3,2,9,-8])
+#  #=> -8
+#
+# returns the last number in the array
+def last_of(array)
+    output = array[-1]
+    return output
+end 
