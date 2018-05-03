41e2ef532e82a05467885b203bb5db2ef6e29099
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-05-03 13:13:20 +0200

    min of two

---
+++ dev/min_of_two.rb
@@ -0,0 +1,22 @@
+# Public: checks which number is smaller
+#
+# num1: the first number
+#num2: second number
+#
+# Examples
+#
+# min_of_two(4,5)
+#  #=> 4
+#
+# returns the smallest
+
+
+def min_of_two(num1, num2)
+    output = num1
+    if num1>num2
+    output = num2
+    end
+    return output
+end
+
+puts min_of_two(8,5)
