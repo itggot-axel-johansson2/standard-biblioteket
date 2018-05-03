6b33bcd7d9535a4d23a1e60de1ddf27b345eb89b
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-05-03 14:44:56 +0200

    poweer

---
+++ lib/power.rb
@@ -0,0 +1,15 @@
+# Public: shows the sum of the first number as base and the second as a exponant
+#
+# number1: input
+# number2: input 2
+#
+# Examples
+#
+# next_number(5,3)
+#  #=> 125
+#
+# returns the number 5 in cube 
+def power(number1, number2)
+    output= number1**number2
+return output
+end
