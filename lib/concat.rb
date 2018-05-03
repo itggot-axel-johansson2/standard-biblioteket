d2d42a9abac93d3eca918d5e22f3c96d72c28ebe
itggot-axel-johansson2 <38528616+itggot-axel-johansson2@users.noreply.github.com>
2018-05-03 15:04:46 +0200

    concat

---
+++ lib/concat.rb
@@ -0,0 +1,15 @@
+# Public: puts togheter two ararys
+#
+# array: input
+# array2: input
+#
+# Examples
+#
+# first_of([1,2,3],[4,5,6])
+#  #=> [1,2,3,4,5,6]
+#
+# returns the two arrays as one
+def concat(array,array2)
+    output = array + array2 
+    return output
+end
